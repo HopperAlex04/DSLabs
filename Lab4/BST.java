27cde7448c11b0fc90ea03c31436d607ad712643
HopperAlex04 <hopperav@appstate.edu>
2025-03-11 15:36:51 -0400

    Lab4 initial commit

---
+++ Lab4/BST.java
@@ -0,0 +1,40 @@
+public class BST
+{
+	private Node root;
+
+	public BST()
+	{
+	}
+	
+	public void insert(int time, Request req)
+	{
+	}
+
+	public Node pred(int time)
+	{
+		return null;
+	}
+
+	public Node succ(int time)
+	{
+		return null;
+	}
+
+	public Node min()
+	{
+		return null;
+	}
+
+	public Node max()
+	{
+		return null;
+	}
+
+	public void delete(int time)
+	{
+	}
+
+	public void print()
+	{
+	}
+}
