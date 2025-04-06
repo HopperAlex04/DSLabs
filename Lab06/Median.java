dc99af337c3efba307c48847f19dd4e9abdbe82f
HopperAlex04 <hopperalex04@gmail.com>
2025-04-06 15:26:31 -0400

    Began Online Median Finding

--- Lab06/Median.java
+++ Lab06/Median.java
@@ -16,10 +16,18 @@ public class Median
 			{
 				v = scan.nextInt();
 				// TODO: insert value v into the dataset
+				if (-v <= small.find_min()) small.insert(-v);
+				else if (v >= large.find_min()) large.insert(v);
+				else small.insert(-v);
+				if (small.size() > large.size()) large.insert(-small.remove_min());
+				if (small.size() < large.size()) small.insert(-large.remove_min());
 			}
 			else if (command == 'q')
 			{
 				// TODO: calculate the median, store it in m
+				if (small.size() == large.size()) m = (-small.find_min() + large.find_min()) / 2;
+				else if (small.size() < large.size()) m = large.find_min();
+				else if (small.size() > large.size()) m = -small.find_min();
 				System.out.println(m);
 			}
 			else break;
